287cf613ab648060086ee16be4985dffa791aeb7
s3mical0n <s3mical0n@DESKTOP-28E8IAV>
2024-09-10 16:03:05 +0330

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace MassSplit
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnBrowseInput = new Button();
+            btnSaveAs = new Button();
+            btnSplit = new Button();
+            btnCalculate = new Button();
+            btnReset = new Button();
+            txtInput = new TextBox();
+            txtOutput = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            nudThread = new NumericUpDown();
+            label3 = new Label();
+            groupBox1 = new GroupBox();
+            lblElapsed = new Label();
+            lblRemaining = new Label();
+            label7 = new Label();
+            lblThread = new Label();
+            label6 = new Label();
+            lblNumbers = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            ((System.ComponentModel.ISupportInitialize)nudThread).BeginInit();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // btnBrowseInput
+            // 
+            btnBrowseInput.Location = new Point(269, 12);
+            btnBrowseInput.Name = "btnBrowseInput";
+            btnBrowseInput.Size = new Size(34, 23);
+            btnBrowseInput.TabIndex = 0;
+            btnBrowseInput.Text = "...";
+            btnBrowseInput.UseVisualStyleBackColor = true;
+            // 
+            // btnSaveAs
+            // 
+            btnSaveAs.Location = new Point(269, 43);
+            btnSaveAs.Name = "btnSaveAs";
+            btnSaveAs.Size = new Size(34, 23);
+            btnSaveAs.TabIndex = 1;
+            btnSaveAs.Text = "...";
+            btnSaveAs.UseVisualStyleBackColor = true;
+            // 
+            // btnSplit
+            // 
+            btnSplit.Location = new Point(208, 72);
+            btnSplit.Name = "btnSplit";
+            btnSplit.Size = new Size(95, 72);
+            btnSplit.TabIndex = 2;
+            btnSplit.Text = "Start Split";
+            btnSplit.UseVisualStyleBackColor = true;
+            btnSplit.Click += btnSplit_Click;
+            // 
+            // btnCalculate
+            // 
+            btnCalculate.Location = new Point(89, 112);
+            btnCalculate.Name = "btnCalculate";
+            btnCalculate.Size = new Size(109, 32);
+            btnCalculate.TabIndex = 3;
+            btnCalculate.Text = "Calculate IPs";
+            btnCalculate.UseVisualStyleBackColor = true;
+            // 
+            // btnReset
+            // 
+            btnReset.Location = new Point(8, 112);
+            btnReset.Name = "btnReset";
+            btnReset.Size = new Size(75, 32);
+            btnReset.TabIndex = 4;
+            btnReset.Text = "Reset";
+            btnReset.UseVisualStyleBackColor = true;
+            // 
+            // txtInput
+            // 
+            txtInput.Location = new Point(76, 12);
+            txtInput.Name = "txtInput";
+            txtInput.Size = new Size(187, 23);
+            txtInput.TabIndex = 5;
+            // 
+            // txtOutput
+            // 
+            txtOutput.Location = new Point(76, 43);
+            txtOutput.Name = "txtOutput";
+            txtOutput.Size = new Size(187, 23);
+            txtOutput.TabIndex = 6;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(8, 17);
+            label1.Name = "label1";
+            label1.Size = new Size(62, 15);
+            label1.TabIndex = 7;
+            label1.Text = "Input IPs : ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(8, 46);
+            label2.Name = "label2";
+            label2.Size = new Size(56, 15);
+            label2.TabIndex = 8;
+            label2.Text = "Save As : ";
+            // 
+            // nudThread
+            // 
+            nudThread.Location = new Point(144, 78);
+            nudThread.Maximum = new decimal(new int[] { 10000, 0, 0, 0 });
+            nudThread.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            nudThread.Name = "nudThread";
+            nudThread.Size = new Size(54, 23);
+            nudThread.TabIndex = 9;
+            nudThread.Value = new decimal(new int[] { 350, 0, 0, 0 });
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(8, 82);
+            label3.Name = "label3";
+            label3.Size = new Size(113, 15);
+            label3.TabIndex = 10;
+            label3.Text = "Number of Thread : ";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(lblElapsed);
+            groupBox1.Controls.Add(lblRemaining);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(lblThread);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(lblNumbers);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Location = new Point(9, 152);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(294, 110);
+            groupBox1.TabIndex = 11;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Statistic";
+            // 
+            // lblElapsed
+            // 
+            lblElapsed.AutoSize = true;
+            lblElapsed.Location = new Point(97, 83);
+            lblElapsed.Name = "lblElapsed";
+            lblElapsed.Size = new Size(49, 15);
+            lblElapsed.TabIndex = 0;
+            lblElapsed.Text = "00:00:00";
+            // 
+            // lblRemaining
+            // 
+            lblRemaining.AutoSize = true;
+            lblRemaining.ForeColor = Color.Red;
+            lblRemaining.Location = new Point(97, 43);
+            lblRemaining.Name = "lblRemaining";
+            lblRemaining.Size = new Size(13, 15);
+            lblRemaining.TabIndex = 0;
+            lblRemaining.Text = "0";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(9, 83);
+            label7.Name = "label7";
+            label7.Size = new Size(82, 15);
+            label7.TabIndex = 0;
+            label7.Text = "Elapsed Time :";
+            // 
+            // lblThread
+            // 
+            lblThread.AutoSize = true;
+            lblThread.Location = new Point(97, 63);
+            lblThread.Name = "lblThread";
+            lblThread.Size = new Size(25, 15);
+            lblThread.TabIndex = 0;
+            lblThread.Text = "350";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(9, 43);
+            label6.Name = "label6";
+            label6.Size = new Size(73, 15);
+            label6.TabIndex = 0;
+            label6.Text = "Remaining : ";
+            // 
+            // lblNumbers
+            // 
+            lblNumbers.AutoSize = true;
+            lblNumbers.ForeColor = Color.Green;
+            lblNumbers.Location = new Point(97, 23);
+            lblNumbers.Name = "lblNumbers";
+            lblNumbers.Size = new Size(13, 15);
+            lblNumbers.TabIndex = 0;
+            lblNumbers.Text = "0";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(9, 63);
+            label5.Name = "label5";
+            label5.Size = new Size(57, 15);
+            label5.TabIndex = 0;
+            label5.Text = "Threads : ";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(9, 23);
+            label4.Name = "label4";
+            label4.Size = new Size(48, 15);
+            label4.TabIndex = 0;
+            label4.Text = "All IPs : ";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            ClientSize = new Size(312, 271);
+            Controls.Add(groupBox1);
+            Controls.Add(label3);
+            Controls.Add(nudThread);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtOutput);
+            Controls.Add(txtInput);
+            Controls.Add(btnReset);
+            Controls.Add(btnCalculate);
+            Controls.Add(btnSplit);
+            Controls.Add(btnSaveAs);
+            Controls.Add(btnBrowseInput);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            MdiChildrenMinimizedAnchorBottom = false;
+            MinimizeBox = false;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "MassSplit ~ Coded By s3nat0r";
+            ((System.ComponentModel.ISupportInitialize)nudThread).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnBrowseInput;
+        private Button btnSaveAs;
+        private Button btnSplit;
+        private Button btnCalculate;
+        private Button btnReset;
+        private TextBox txtInput;
+        private TextBox txtOutput;
+        private Label label1;
+        private Label label2;
+        private NumericUpDown nudThread;
+        private Label label3;
+        private GroupBox groupBox1;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label lblElapsed;
+        private Label lblRemaining;
+        private Label lblThread;
+        private Label lblNumbers;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,294 @@
+﻿/**************************                     
+ * Developer : s3nat0r                       
+ * Date : 2024/09/10                    
+ * Version : 1.0.0.0                    
+ * Description : A program to analyze and sort IP:PORT, which is saved as a list from the output of Masscan software
+It also has the ability to process heavy text files                     
+ **************************/
+
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MassSplit
+{
+    public partial class Form1 : Form
+    {
+        private string inputFilePath = "";
+        private string outputFilePath = "";
+        private static readonly object fileLock = new object();
+        private CancellationTokenSource cancellationTokenSource;
+        private System.Timers.Timer elapsedTimeTimer;
+        private DateTime startTime;
+        public Form1()
+        {
+            InitializeComponent();
+            nudThread.Value = 350;
+            nudThread.ValueChanged += (s, e) => lblThread.Text = nudThread.Value.ToString();
+            btnBrowseInput.Click += btnBrowseInput_Click;
+            btnSaveAs.Click += btnSaveAs_Click;
+            btnSplit.Click += btnSplit_Click;
+            btnCalculate.Click += btnCalculate_Click;
+            btnReset.Click += btnReset_Click;
+            lblNumbers.Text = "0";
+            lblRemaining.Text = "0";
+            lblElapsed.Text = "00:00:00";
+            elapsedTimeTimer = new System.Timers.Timer
+            {
+                Interval = 1000
+            };
+            elapsedTimeTimer.Elapsed += (s, e) =>
+            {
+                if (startTime != DateTime.MinValue)
+                {
+                    TimeSpan elapsed = DateTime.Now - startTime;
+                    if (lblElapsed.InvokeRequired)
+                    {
+                        lblElapsed.Invoke(new Action(() =>
+                        {
+                            lblElapsed.Text = elapsed.ToString(@"hh\:mm\:ss");
+                        }));
+                    }
+                    else
+                    {
+                        lblElapsed.Text = elapsed.ToString(@"hh\:mm\:ss");
+                    }
+                }
+            };
+
+        }
+        private async Task<int> CalculateTotalLinesAsync(string filePath, IProgress<int> progress, CancellationToken token)
+        {
+            int totalLines = 0;
+            const int batchSize = 1000;
+            int linesInBatch = 0;
+
+            using (var reader = new StreamReader(filePath))
+            {
+                string line;
+                while ((line = await reader.ReadLineAsync()) != null)
+                {
+                    token.ThrowIfCancellationRequested();
+                    totalLines++;
+                    linesInBatch++;
+                    if (linesInBatch >= batchSize)
+                    {
+                        linesInBatch = 0;
+                        progress?.Report(totalLines);
+                    }
+                }
+            }
+            progress?.Report(totalLines);
+            return totalLines;
+        }
+        private void btnBrowseInput_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    inputFilePath = openFileDialog.FileName;
+                    txtInput.Text = inputFilePath;
+                }
+            }
+        }
+        private async void btnSaveAs_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+                saveFileDialog.DefaultExt = "txt";
+                saveFileDialog.AddExtension = true;
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    outputFilePath = saveFileDialog.FileName;
+                    if (!outputFilePath.EndsWith(".txt", StringComparison.OrdinalIgnoreCase))
+                    {
+                        outputFilePath += ".txt";
+                    }
+
+                    txtOutput.Text = outputFilePath;
+                }
+            }
+        }
+        private async void btnCalculate_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtInput.Text))
+            {
+                MessageBox.Show("Please select an input file.");
+                return;
+            }
+
+            lblNumbers.Text = "Calculating...";
+            var progress = new Progress<int>(count =>
+            {
+                lblNumbers.Text = $"Total Lines: {count}";
+            });
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+            try
+            {
+                int totalLines = await CalculateTotalLinesAsync(txtInput.Text, progress, cts.Token);
+                lblNumbers.Text = $"Total Lines: {totalLines}";
+            }
+            catch (OperationCanceledException)
+            {
+                lblNumbers.Text = "Calculation was canceled.";
+            }
+        }
+
+        private int? totalLineCount = null;
+
+        private async void btnSplit_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtInput.Text) || string.IsNullOrEmpty(txtOutput.Text))
+            {
+                MessageBox.Show("Please select input and output files.");
+                return;
+            }
+            startTime = DateTime.Now;
+            elapsedTimeTimer.Start();
+            if (totalLineCount == null)
+            {
+                lblRemaining.Text = "Calculating total lines...";
+                var progress = new Progress<int>(count =>
+                {
+                    lblNumbers.Text = $"Total Lines: {count}";
+                });
+
+                CancellationTokenSource cts = new CancellationTokenSource();
+                try
+                {
+                    totalLineCount = await CalculateTotalLinesAsync(txtInput.Text, progress, cts.Token);
+                    lblNumbers.Text = $"Total Lines: {totalLineCount}";
+                }
+                catch (OperationCanceledException)
+                {
+                    lblRemaining.Text = "Operation was canceled.";
+                    return;
+                }
+            }
+            else
+            {
+                lblRemaining.Text = $"Total Lines (Cached): {totalLineCount}";
+            }
+            lblRemaining.Text = "Processing file...";
+            try
+            {
+                await ProcessFileAsync(txtInput.Text, txtOutput.Text, (int)nudThread.Value, new Progress<(int processed, int remaining)>(update =>
+                {
+                    lblRemaining.Text = $"{update.processed}";
+                }), CancellationToken.None);
+            }
+            finally
+            {
+                elapsedTimeTimer.Stop();
+            }
+        }
+
+
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtInput.Text = "";
+            txtOutput.Text = "";
+            lblNumbers.Text = "0";
+            lblThread.Text = "0";
+            lblRemaining.Text = "0";
+            lblElapsed.Text = "00:00:00";
+            inputFilePath = "";
+            outputFilePath = "";
+            cancellationTokenSource?.Cancel();
+        }
+
+        private async Task ProcessFileAsync(string inputFile, string outputFile, int numThreads, IProgress<(int processed, int remaining)> progress, CancellationToken token)
+        {
+            const int batchSize = 1000;
+            int totalLines = 0;
+            int linesProcessed = 0;
+
+            var outputLines = new List<string>();
+
+            try
+            {
+                using (var reader = new StreamReader(inputFile))
+                using (var writer = new StreamWriter(outputFile, append: false))
+                {
+                    string line;
+                    while ((line = await reader.ReadLineAsync()) != null)
+                    {
+                        token.ThrowIfCancellationRequested();
+                        if (line.Contains("open tcp"))
+                        {
+                            var parts = line.Split(' ');
+                            if (parts.Length >= 4)
+                            {
+                                string ip = parts[3];
+                                string port = parts[2];
+                                outputLines.Add($"{ip}:{port}");
+                            }
+                        }
+
+                        linesProcessed++;
+                        totalLines++;
+
+                        if (linesProcessed >= batchSize)
+                        {
+                            await WriteBatchAsync(writer, outputLines);
+                            outputLines.Clear();
+                            progress?.Report((totalLines, totalLines - linesProcessed));
+                            linesProcessed = 0;
+                        }
+                    }
+
+                    if (outputLines.Count > 0)
+                    {
+                        await WriteBatchAsync(writer, outputLines);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show($"Error processing file: {ex.Message}");
+            }
+
+            progress?.Report((totalLines, 0));
+        }
+
+        private async Task WriteBatchAsync(StreamWriter writer, List<string> lines)
+        {
+            try
+            {
+                await writer.WriteLineAsync(string.Join(Environment.NewLine, lines));
+            }
+            catch (IOException ioEx)
+            {
+                //MessageBox.Show($"File write error: {ioEx.Message}");
+            }
+        }
+
+
+
+        private void UpdateUI(int processed, int remaining)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action(() =>
+                {
+                    lblRemaining.Text = remaining.ToString();
+                }));
+            }
+            else
+            {
+                lblRemaining.Text = remaining.ToString();
+            }
+        }
+
+    }
+}
